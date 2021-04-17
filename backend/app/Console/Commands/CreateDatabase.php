9aab57a0567b9c4dd9bcb6b693f4397d0ec04205
adal6a <adalgarcia17@gmail.com>
2021-04-17 12:41:07 -0600

    configuracion de bases de datos

---
+++ backend/app/Console/Commands/CreateDatabase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Crea el arhivo de base de datos bluebank.sqlite';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        touch(database_path() .'/bluebank.sqlite');
+
+        return 0;
+    }
+}
